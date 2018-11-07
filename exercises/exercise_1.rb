4f11b76931e36d724c0aa712484878f97b828672
Andrea <andreamayhirji@gmail.com>
2018-11-07 07:36:29 +0000

    finished excercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,5 +1,14 @@
 require_relative '../setup'
 
+class Store < ActiveRecord::Base
+  end
+
+burnaby = Store.create name: 'Burnaby store', annual_revenue: 300000, mens_apparel: true, womens_apparel: true
+richmond = Store.create name: 'Richmond store', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true
+gastown = Store.create name: 'Gastown store', annual_revenue: 190000, mens_apparel: true, womens_apparel: false
+
+puts Store.count
+
 puts "Exercise 1"
 puts "----------"
 
