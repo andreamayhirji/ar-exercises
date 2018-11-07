1cc85883e2d5394e068112222dff7d2a12a2390a
Andrea <andreamayhirji@gmail.com>
2018-11-07 08:57:06 +0000

    Finished excercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,13 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+p @store2.employees.create(first_name: "Sizzle", last_name: "Snap", hourly_rate: 60)
+error_store = Store.create name: 'Cat Shop', annual_revenue: 200_000, mens_apparel: true, womens_apparel: true
+
+puts "Provide a store name:"
+store_name = $stdin.gets.chomp
+
+user_store = Store.create name: store_name
+
+p user_store.valid?
+puts @users_store.errors.messages
